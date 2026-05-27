be4ab82f5c909a8fd52667dd5f8422a43b9c7ea4
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-05-27 18:41:43 -0400

    feat: add login and register express routes

---
+++ apps/chat-server/src/routes/auth.ts
@@ -0,0 +1,58 @@
+import express, { Request, Response } from 'express';
+import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { createUser, findUserByEmail } from '../db';
+
+const router = express.Router();
+
+router.route('/register').post(async (req: Request, res: Response) => {
+  try {
+    const { email, username, password } = req.body;
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const newUser = await createUser(username, email, hashedPassword);
+
+    if (!process.env.JWT_SECRET)
+      return res.status(500).json({ error: 'Server misconfiguration' });
+
+    const token = jwt.sign(
+      {
+        id: newUser.id,
+        username: newUser.username,
+        email: newUser.email,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '7d' },
+    );
+
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(500).json({ error: 'Registration Failed' });
+  }
+});
+
+router.route('/login').post(async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await findUserByEmail(email);
+    if (!user) return res.status(401).json({ error: 'Invalid credentials' });
+    const passwordMatch = await bcryptjs.compare(password, user.password_hash);
+
+    if (!process.env.JWT_SECRET)
+      return res.status(500).json({ error: 'Server misconfiguration' });
+
+    if (passwordMatch) {
+      const token = jwt.sign(
+        { id: user.id, username: user.username, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' },
+      );
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ error: 'Unauthorized' });
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'Login failed' });
+  }
+});
+
+module.exports = { router };
--- apps/chat-server/src/server.ts
+++ apps/chat-server/src/server.ts
@@ -1,15 +1,23 @@
 import { Server } from 'socket.io';
 const http = require('http');
 const express = require('express');
+const cors = require('cors');
 import type {
   ClientToServerEvents,
   ServerToClientEvents,
 } from '@gather/socket-events';
+const { authMiddleware } = require('./middleware');
+const { registerHandlers } = require('./handlers');
 
 const app = express();
+app.use(express.json);
+app.use(cors());
 const httpServer = http.createServer(app);
 const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer, {
   cors: { origin: '*' },
 });
+
+io.use(authMiddleware);
+registerHandlers();
 
 module.exports = { app, httpServer, io };
